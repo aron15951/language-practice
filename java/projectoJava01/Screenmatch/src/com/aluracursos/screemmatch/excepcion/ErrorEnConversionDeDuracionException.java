d155a8b64015f0e9effc15a95a3d5b2ce0b37604
aron15951 <aronchoque72@gmail.com>
2024-10-11 08:21:29 -0500

    culminacion de reto 02 alura y   refactor de unj ejercicio de c++

---
+++ java/projectoJava01/Screenmatch/src/com/aluracursos/screemmatch/excepcion/ErrorEnConversionDeDuracionException.java
@@ -0,0 +1,6 @@
+package com.aluracursos.screemmatch.excepcion;
+
+public class ErrorEnConversionDeDuracionException extends Throwable {
+    public ErrorEnConversionDeDuracionException(String s) {
+    }
+}
---
+++ java/projectoJava01/Screenmatch/src/com/aluracursos/screemmatch/modelos/TituloOmdb.java
@@ -0,0 +1,4 @@
+package com.aluracursos.screemmatch.modelos;
+
+public record TituloOmdb() {
+}
